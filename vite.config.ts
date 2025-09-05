cafba68aff3b16d3096e3b59c64e71bc7bc93ee5
manpreet singh <tech@acharyaventures.com>
2025-09-05 19:47:08 +0530

    fi

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-  server: {
-    port: 3000,
-    open: true
+    outDir: "dist"
   }
 });
