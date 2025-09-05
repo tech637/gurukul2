4a64105946eee94bfe447ec686adaff4e1b98d0c
manpreet singh <tech@acharyaventures.com>
2025-09-05 19:14:40 +0530

    fixing npm issue

--- main.tsx
+++ main.tsx
@@ -2,15 +2,12 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Example from './src/App.StartupGurukul'
 import KombaiWrapper from './KombaiWrapper'
-import ErrorBoundary from '@kombai/react-error-boundary'
 import './src/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <KombaiWrapper>
-        <Example />
-      </KombaiWrapper>
-    </ErrorBoundary>
+    <KombaiWrapper>
+      <Example />
+    </KombaiWrapper>
   </StrictMode>,
 )
